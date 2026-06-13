b30d3e8e4df1af67e6c547f33ddbcbc585f505f8
JesLopMar <jeslopmar2002@gmail.com>
2026-06-13 13:49:01 +0200

    feat: incluir traducción a japonés, adaptar y mejorar configuración

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -2,26 +2,28 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationDE from '../assets/locales/de/translation.json';
-import translationEN from '../assets/locales/en/translation.json';
-import translationES from '../assets/locales/es/translation.json';
-import translationFR from '../assets/locales/fr/translation.json';
-import translationIT from '../assets/locales/it/translation.json';
-import translationPT from '../assets/locales/pt/translation.json';
+import translationDE from '@/assets/locales/de/translation.json';
+import translationEN from '@/assets/locales/en/translation.json';
+import translationES from '@/assets/locales/es/translation.json';
+import translationFR from '@/assets/locales/fr/translation.json';
+import translationIT from '@/assets/locales/it/translation.json';
+import translationPT from '@/assets/locales/pt/translation.json';
+import translationJA from '@/assets/locales/ja/translation.json';
 
-import wikiDE from '../assets/locales/de/wiki.json';
-import wikiEN from '../assets/locales/en/wiki.json';
-import wikiES from '../assets/locales/es/wiki.json';
-import wikiFR from '../assets/locales/fr/wiki.json';
-import wikiIT from '../assets/locales/it/wiki.json';
-import wikiPT from '../assets/locales/pt/wiki.json';
+import wikiDE from '@/assets/locales/de/wiki.json';
+import wikiEN from '@/assets/locales/en/wiki.json';
+import wikiES from '@/assets/locales/es/wiki.json';
+import wikiFR from '@/assets/locales/fr/wiki.json';
+import wikiIT from '@/assets/locales/it/wiki.json';
+import wikiPT from '@/assets/locales/pt/wiki.json';
+import wikiJA from '@/assets/locales/ja/wiki.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    supportedLngs: ['de', 'en', 'es', 'fr', 'it', 'pt'],
+    supportedLngs: ['de', 'en', 'es', 'fr', 'it', 'pt', 'ja'],
     ns: ['translation', 'wiki'],
     defaultNS: 'translation',
     debug: import.meta.env.DEV,
@@ -33,6 +35,7 @@ i18n
       fr: { translation: translationFR, wiki: wikiFR },
       it: { translation: translationIT, wiki: wikiIT },
       pt: { translation: translationPT, wiki: wikiPT },
+      ja: { translation: translationJA, wiki: wikiJA },
     },
   });
 
