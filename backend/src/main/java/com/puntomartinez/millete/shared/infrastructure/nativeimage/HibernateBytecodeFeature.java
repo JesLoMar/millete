535e7e3f155e985e77c316cc64c03737d80f3b40
JesLopMar <jeslopmar2002@gmail.com>
2026-09-07 21:55:30 +0200

    fix: corregir bd

--- backend/src/main/java/com/puntomartinez/millete/shared/infrastructure/nativeimage/HibernateBytecodeFeature.java
+++
@@ -1,6 +0,0 @@
-package com.puntomartinez.millete.shared.infrastructure.nativeimage;
-
-import org.graalvm.nativeimage.hosted.Feature;
-
-public final class HibernateBytecodeFeature implements Feature {
-}
